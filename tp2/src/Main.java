c03c79be6babd026f13733c85058b698292b67c7
izaratedev <izarate.developer@gmail.com>
2026-02-19 18:46:32 -0300

    Se hizo el ejercicio 1 y 2

--- tp2/src/Main.java
+++ tp2/src/Main.java
@@ -2,6 +2,34 @@ package src;
 
 import java.util.ArrayList;
 
+
+//--------------------------------------------------Trabajo práctico N° 2 - Recursión y ordenamiento---------------------------------------------------------------------//
+
+/*
+Trabajo práctico N° 2 - Recursión y ordenamiento
+Primera parte
+Ejercicio 1
+Implemente un algoritmo recursivo que determine si un arreglo de tamaño N está ordenado y
+responda:
+1. ¿Qué complejidad O tiene? (La complejidad en el peor caso)
+2. ¿Trae algún problema hacerlo recursivo? ¿Cuál?
+3. ¿Qué cambiaría si la estructura fuera una lista en lugar de un arreglo?
+
+Ejercicio 2
+Implemente un algoritmo recursivo para buscar un elemento en un arreglo ordenado
+ascendentemente.
+
+Ejercicio 3
+Implemente un algoritmo recursivo que convierta un número en notación decimal a su
+equivalente en notación binaria.
+Recordatorio, por ejemplo convertir el 26 a binario:
+
+Ejercicio 4
+Implemente un algoritmo recursivo que presente los primeros N términos de la secuencia de
+Fibonacci.
+Por ej. los 6 primeros términos son: 0 1 1 2 3 5
+ */
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,5 +42,59 @@ public class Main {
         list.add(4);
 
         System.out.println(list);
+
+        if(isOrdered(list, 0)){
+            System.out.println("Esta ordenada");
+        }else {
+            System.out.println("No esta ordenada");
+        }
+
+        System.out.println(searchElement(list, 0, 1));
+    }
+
+
+    public static boolean isOrdered(ArrayList<Integer> list, int pos){
+
+        if(pos == list.size()-1){
+            return true;
+        } else if (list.get(pos) < list.get(pos+1)) {
+            return isOrdered(list, pos + 1);
+        }else {
+            return false;
+        }
+    }
+
+    /*
+    Ejercicio 2
+    Implemente un algoritmo recursivo para buscar un elemento en un arreglo ordenado
+    ascendentemente.
+    */
+    //Esta es una forma pero es On
+    public static boolean searchElement(ArrayList<Integer> list, int pos, int element) {
+
+        if(pos == list.size() || list.get(pos) > element){
+            return false;
+        }
+
+        if(list.get(pos) != element){
+            return searchElement(list, pos + 1, element);
+        } else if (list.get(pos) == element) {
+            return true;
+        }else {
+            return false;
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
