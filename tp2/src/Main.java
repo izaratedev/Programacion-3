5a09bb3bee60011d296dbaee0f775f485cf39ba4
izaratedev <izarate.developer@gmail.com>
2026-02-26 15:37:21 -0300

    Resuelve ejercicios del tp2 de recursion y ordenamiento

--- tp2/src/Main.java
+++ tp2/src/Main.java
@@ -49,7 +49,23 @@ public class Main {
             System.out.println("No esta ordenada");
         }
 
-        System.out.println(searchElement(list, 0, 1));
+        // System.out.println(searchElement(list, 0, 1));
+        //System.out.println(searchElement(0, list.size()-1, 8,list));
+
+        convertBinary(26);
+
+        System.out.println(" ");
+
+        ArrayList<Integer> binario = new ArrayList<>();
+
+        binario = cconvertBinary(26, binario);
+
+        System.out.println(binario);
+
+        fibonacci(6);
+
+
+
     }
 
 
@@ -69,22 +85,174 @@ public class Main {
     Implemente un algoritmo recursivo para buscar un elemento en un arreglo ordenado
     ascendentemente.
     */
-    //Esta es una forma pero es On
-    public static boolean searchElement(ArrayList<Integer> list, int pos, int element) {
+    //Esta es una forma pero es O(n)
+    public static int searchElement(ArrayList<Integer> list, int pos, int element) {
 
         if(pos == list.size() || list.get(pos) > element){
-            return false;
+            return -1;
         }
 
         if(list.get(pos) != element){
             return searchElement(list, pos + 1, element);
         } else if (list.get(pos) == element) {
-            return true;
+            return pos;
         }else {
-            return false;
+            return -1;
         }
 
     }
+
+    //Esta forma de buscar el elemento en una lista ordenada es mas eficiente porque la complejidad computacional es O(log2n)
+    public static int searchElement(int start, int end, int element, ArrayList<Integer> list) {
+
+
+        if(start > end){
+            return -1;
+        }else{
+           int medio = (start + end) / 2;
+
+            if(list.get(medio) > element){
+                return searchElement(start, medio-1, element, list);
+            }else if(list.get(medio) < element){
+                return searchElement(medio+1, end, element, list);
+            }else {
+                return medio;
+            }
+        }
+
+    }
+
+    /*
+    Ejercicio 3
+    Implemente un algoritmo recursivo que convierta un número en notación decimal a su
+    equivalente en notación binaria.
+    Recordatorio, por ejemplo convertir el 26 a binario:
+
+     */
+
+    public static String convertBinary(int x){
+        int resto;
+        if(x < 2){
+           return String.valueOf(x);
+        }else {
+            return convertBinary(x / 2) + (x % 2);
+        }
+    }
+
+    public static ArrayList<Integer> cconvertBinary(int x, ArrayList<Integer> list){
+
+        int resto;
+        if(x < 2){
+            list.add(x);
+        }else {
+            resto = x % 2;
+            cconvertBinary(x / 2, list);
+            list.add(resto);
+        }
+        return list;
+    }
+    /*
+    Ejercicio 4
+    Implemente un algoritmo recursivo que presente los primeros N términos de la secuencia de
+    Fibonacci.
+    Por ej. los 6 primeros términos son: 0 1 1 2 3 5
+            */
+    public static void fibonacci(int n){
+
+        imprimirSecuenciaFibonacci(0, 1, n);
+    }
+
+    public static void imprimirSecuenciaFibonacci(int a, int b, int n){
+        if(n == 0){
+            return;
+        }else {
+            System.out.print(a + " ");
+            imprimirSecuenciaFibonacci(b, a+b, n-1);
+        }
+    }
+    /*
+    Ejercicio 5
+    Dado un arreglo ordenado de números distintos A se desea construir un algoritmo que
+    determine si alguno de los elementos de dicho arreglo contiene un valor igual a la posición en la
+    cuál se encuentra, es decir, A[i] = i.
+    1. Construir un algoritmo recursivo que responda a dicha verificación.
+    2. Mostrar la pila de ejecución para la invocación algoritmo([-3, -1, 0, 2, 4, 6, 10])
+     */
+
+   //De esta forma encontrar el valor es O(n)
+    public static boolean mismoValorPos(ArrayList<Integer> list, int pos){
+
+        if(pos > list.size()-1){
+            return false;
+        }else {
+            if(list.get(pos) == pos){
+                return true;
+            }else {
+                return mismoValorPos(list, pos + 1);
+            }
+        }
+    }
+
+    /*
+    Ejercicio 6
+    Implemente un algoritmo de ordenamiento por selección en un arreglo.
+    Implemente un algoritmo de ordenamiento por burbujeo en un arreglo.
+    ● ¿Qué complejidad big-O tienen estos algoritmos?
+     */
+
+    public static void ordenamientoSeleccion(ArrayList<Integer> list){
+        int fin = list.size();
+        for(int i = 0; i < fin -1 ; i++){
+            int posMin = i;
+
+            for(int j = i + 1; j < fin; j++){
+                if(list.get(posMin) > list.get(j)) {
+                    posMin = j;
+                }
+            }
+            int temp = list.get(i);
+            list.set(i, list.get(posMin));
+            list.set(posMin, temp);
+
+        }
+    }
+
+    public static void ordenamientoBurbujeo(ArrayList<Integer> list){
+        int fin = list.size();
+
+        for(int i = 0; i < fin -1 ; i++){
+
+            for(int j = 0; j < fin-1-i; j++){
+
+                if(list.get(j) > list.get(j+1)) {
+                    int temp = list.get(j);
+                    list.set(j, list.get(j+1));
+                    list.set(j+1, temp);
+                }
+            }
+        }
+    }
+
+    public static void ordenamientoBurbujeoPro(ArrayList<Integer> list){
+        int n = list.size();
+        int i = 0;
+        boolean swapped = true;
+        while(swapped){
+            swapped = false;
+
+            for(int j = 0; j < n-1-i; j++){
+
+                if(list.get(j) > list.get(j+1)) {
+                    swapped = true;
+                    int temp = list.get(j);
+                    list.set(j, list.get(j+1));
+                    list.set(j+1, temp);
+                }
+            }
+            i++;
+        }
+    }
+
 }
 
 
